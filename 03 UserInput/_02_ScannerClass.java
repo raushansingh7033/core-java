b062718686781aa556f3c2296873364a70b2c79c
Raushan Singh <raushan786267@gmail.com>
2026-03-05 16:41:58 +0530

    using scanner class

---
+++ 03 UserInput/_02_ScannerClass.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class _02_ScannerClass  {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter materials used :");
+        while (sc.hasNextLine()) {
+            String data = sc.nextLine();
+            if (data.isEmpty()) {
+                break; // Exit the loop if an empty line is entered
+            }
+            System.out.println(" Material is :" + data);
+        }
+
+        System.out.println("byeee");
+        // int i=1;
+        // while (sc.hasNext())
+        // {
+        // System.out.println( (i++) +" "+sc.nextLine());
+        // }
+        sc.close();
+    }
+}
