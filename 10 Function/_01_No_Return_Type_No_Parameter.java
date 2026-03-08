52bdbbcc88585b03f55ef981c946cd3d3fb7e548
Raushan Singh <raushan786267@gmail.com>
2026-03-08 08:56:04 +0530

    learnig method in java

---
+++ 10 Function/_01_No_Return_Type_No_Parameter.java
@@ -0,0 +1,11 @@
+public class _01_No_Return_Type_No_Parameter {
+    public static void show() {
+        System.out.println("Java Method Example");
+    }
+
+    public static void main(String[] args) {
+
+        show();
+
+    }
+}
