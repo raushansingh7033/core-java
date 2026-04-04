2742383a5c2af5d786ccee2e8e998544eb44b3b6
raushansingh7033 <raushan786267@gmail.com>
2026-04-04 12:16:17 +0530

    conditional statement practice

---
+++ 05 ConditionalStatement/_01_If.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class _01_If {
+ public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter your age");
+    int age=sc.nextInt();
+    if(age>=18){
+        System.out.println("you can vote");
+    }
+ }   
+}
